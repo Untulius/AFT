5076fe49d3669c16e62b74c03c8b96c47e66bf31
Victor Untulius <untulius@gmail.com>
2020-02-13 23:22:30 +0300

    Добавил первые два задания из lesson1

---
+++ JavaCore/src/Lesson1/DescriptionOfNumber.java
@@ -0,0 +1,24 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class DescriptionOfNumber {
+    public static void main(String[] args) {
+        int a;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        a = scanner.nextInt();
+
+        if (a==0) System.out.println("Введенное вами число является нулем");
+            else {
+                System.out.print("Введенное вами число ");
+                if (a % 2 == 0) System.out.print("чётное, ");
+                    else System.out.print("нечетное, ");
+                if (a<0) System.out.println("отрицательное");
+                    else System.out.println("положительное");
+        }
+
+    }
+
+
+}
---
+++ JavaCore/src/Lesson1/Minimal.java
@@ -0,0 +1,27 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class Minimal {
+
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите значение первого числа a =");
+        a = scanner.nextInt();
+
+        System.out.println("Введите значение второго числа: b =");
+        b = scanner.nextInt();
+
+        if (min(a, b)==0) System.out.println("Числа равны");
+            else System.out.println("Минимальное число равно " + min(a, b));
+    }
+
+    public static int min(int a, int b){
+        if (a < b) return a;
+            else if (b < a) return b;
+                else return 0;
+    }
+
+}
