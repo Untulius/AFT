03e3db78e9eb6d8c1d6ec8d9bde9b055347d198a
untulius <untulius@gmail.com>
2020-02-19 17:38:14 +0300

    Task3

--- JavaCore/src/Lesson3/Task3.java
+++ JavaCore/src/Lesson3/Task3.java
@@ -1,7 +1,25 @@
 package Lesson3;
+
+import java.util.Scanner;
+
 /*
 Реализовать расчет и вывод в консоль последовательности чисел
 Фибоначчи до указанного пользователем N, посредством рекурсии
  */
 public class Task3 {
+    public static void main(String[] args) {
+        int N, tmp;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число членов последовательности N: ");
+        N = scanner.nextInt();
+        tmp = 0;
+        System.out.println("Последовательность Фибоначчи: " + fib(N));
+    }
+
+    private static int fib(int N, int tmp)
+    {
+        if (tmp<N){
+
+        }
+    }
 }
